c28662c0b1f8b0945f95daaa7a5a50a87ca9d39b
Yuzdzhan Mehmedov <ynmehmedov@gmail.com>
2023-05-24 09:43:59 +0300

    Removed old files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
